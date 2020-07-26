5a64d5bcd892bc4c179d91ba333544488bf700f0
Yang Siyu <siyuyang1995@gmail.com>
2020-07-26 18:39:58 +0800

    [yang siyu]feat: should return common words space.

--- src/main/java/GrammarExercise.java
+++ src/main/java/GrammarExercise.java
@@ -1,10 +1,12 @@
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GrammarExercise {
     public static void main(String[] args) {
         //需要从命令行读入
-        String firstWordList = "";
-        String secondWordList = "";
+        String firstWordList = args[0];
+        String secondWordList = args[1];
 
         List<String> result = findCommonWordsWithSpace(firstWordList,secondWordList);
         //按要求输出到命令行
@@ -13,6 +15,21 @@ public class GrammarExercise {
 
     public static List<String> findCommonWordsWithSpace(String firstWordList, String secondWordList) {
         //在这编写实现代码
-        return null;
+        List<String> firstList =  Arrays.stream(secondWordList.split(",")).map(String::toUpperCase).map(s->s.replace("", " ").trim()).collect(Collectors.toList());
+        List<String> secondList =  Arrays.stream(firstWordList.split(",")).map(String::toUpperCase).map(s->s.replace("", " ").trim()).collect(Collectors.toList());
+
+        for (String word:firstList){
+            if (!word.matches("[A-Z ]+")) {
+                throw new RuntimeException("input not valid");
+            }
+        }
+        for (String word:secondList){
+            if (!word.matches("[A-Z ]+")) {
+                throw new RuntimeException("input not valid");
+            }
+        }
+
+        List<String> result = firstList.stream().distinct().filter(secondList::contains).collect(Collectors.toList());
+        return result;
     }
 }
